8559aa22857dda8c16e3e2e964793d04c2116462
Your Name <you@example.com>
2023-04-11 17:14:54 +0300

    board

--- 2023kevat_bbti_1600_react_harjoitus/src/tictactoe/Board.jsx
+++ 2023kevat_bbti_1600_react_harjoitus/src/tictactoe/Board.jsx
@@ -1,22 +1,30 @@
 import React from "react";
 import { Square } from "./Square";
-export const Board = ()=>{
+export const Board = ({gameState})=>{
     return(
         <div>
-            <div>
-                <Square/>
-                <Square/>
-                <Square/>
+            <div className="row">
+                <Square className="b-bottom-r"
+                state ={gameState[0]}/>
+                <Square className="b-bottom-r"
+                state ={gameState[1]}/>
+                <Square className="b-bottom"
+                state={gameState[2]}/>
             </div>
-            <div>
-                <Square/>
-                <Square/>
-                <Square/>
+            <div className="row">
+                <Square className="b-bottom-r"
+                state={gameState[3]}/>
+                <Square className="b-bottom-r"
+                state={gameState[4]}/>
+                <Square className="b-bottom"
+                state={gameState[5]}/>
             </div>
-            <div>
-                <Square/>
-                <Square/>
-                <Square/>
+            <div className="row">
+                <Square className="b-right"
+                state={gameState[6]}/>
+                <Square className="b-right"
+                state={gameState[7]}/>
+                <Square state={gameState[8]}/>
             </div>
         </div>
     );
