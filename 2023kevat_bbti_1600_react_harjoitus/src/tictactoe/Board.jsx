cac9724867b12c8da7f5dc70078871d53d7bcaee
Your Name <you@example.com>
2023-04-25 17:00:20 +0300

    voiton tarkastus tehty

--- 2023kevat_bbti_1600_react_harjoitus/src/tictactoe/Board.jsx
+++ 2023kevat_bbti_1600_react_harjoitus/src/tictactoe/Board.jsx
@@ -9,24 +9,40 @@ export const Board = ({gameState, onSquareClick})=>{
                 onClick={()=> onSquareClick(0)}
                 />
                 <Square className="b-bottom-r"
-                state ={gameState[1]}/>
+                state ={gameState[1]}
+                onClick={()=> onSquareClick(1)}
+                />
                 <Square className="b-bottom"
-                state={gameState[2]}/>
+                state={gameState[2]}
+                onClick={()=> onSquareClick(2)}
+                />
             </div>
             <div className="row">
                 <Square className="b-bottom-r"
-                state={gameState[3]}/>
+                state={gameState[3]}
+                onClick={()=> onSquareClick(3)}
+                />
                 <Square className="b-bottom-r"
-                state={gameState[4]}/>
+                state={gameState[4]}
+                onClick={()=> onSquareClick(4)}
+                />
                 <Square className="b-bottom"
-                state={gameState[5]}/>
+                state={gameState[5]}
+                onClick={()=> onSquareClick(5)}
+                />
             </div>
             <div className="row">
                 <Square className="b-right"
-                state={gameState[6]}/>
+                state={gameState[6]}
+                onClick={()=> onSquareClick(6)}
+                />
                 <Square className="b-right"
-                state={gameState[7]}/>
-                <Square state={gameState[8]}/>
+                state={gameState[7]}
+                onClick={()=> onSquareClick(7)}
+                />
+                <Square state={gameState[8]}
+                 onClick={()=> onSquareClick(8)}
+                />
             </div>
         </div>
     );
--- 2023kevat_bbti_1600_react_harjoitus/src/tictactoe/TicTacToe.jsx
+++ 2023kevat_bbti_1600_react_harjoitus/src/tictactoe/TicTacToe.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import './TicTacToe.css';
 import { Board } from "./Board";
 import { useState } from "react";
+import { useEffect } from "react";
 
 const intialBoard=['','','','','','','','','']
 
 export const TicTacToe=()=>{
     const [gameState,setGameState] = useState(intialBoard);
     const [isXTurn, setIsXTurn] = useState(true);
+    const [status, setStatus] = useState('');
 
     const onSquareClick = (index)=>{
         let strings = Array.from(gameState);
@@ -20,11 +22,40 @@ export const TicTacToe=()=>{
         setIsXTurn(!isXTurn);
     }
 
+    useEffect(()=>{
+        const winner = checkWinner();
+        if(winner){
+            setStatus(`Winner: ${winner}`);
+        }
+    },[gameState])
+
+    const checkWinner =()=>{
+        const lines = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6],
+        ];
+        for(let i = 0; i < lines.length; i++){
+            const [a,b,c] = lines[i];
+            if(gameState[a] && gameState[a] === gameState[b]&& gameState[a] === gameState[c])
+            {
+                return gameState[a];
+            }
+        }
+        return null;
+    }
+
     return(
         <div>
             <div className="game">
                 <h1>Tic-Tac-Toe</h1>
                 <Board gameState={gameState} onSquareClick={onSquareClick}/>
+                {status}
             </div>
         </div>
     );
